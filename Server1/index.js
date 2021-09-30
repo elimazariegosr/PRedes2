b1496c1b7e0df76f6f4c5128b703f32ab540bf86
Eli Mazariegos <elimazariegosr@gmail.com>
2021-09-29 23:40:19 -0600

    asd

--- Server1/index.js
+++ Server1/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 var body_parser = require('body-parser')
-var mysql = require('mysql');
+var mysql = require('mysql2');
 const axios = require('axios')
 const app = express()
 const port = 5006
